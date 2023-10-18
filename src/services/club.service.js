f6b1228fdbbd7650f1f702daad6ac80a921a2520
Vitor Nogueira <vslnogueira96@gmail.com>
2023-10-18 15:55:04 -0300

    feat: creating service get partner by id

--- src/services/club.service.js
+++ src/services/club.service.js
@@ -8,6 +8,15 @@ const getAllPartners = async () => {
   return { type: null, message: allPartners, status: 200 };
 };
 
+const getPartnerById = async (id) => {
+  const partnerById = await model.getPartnerById(id);
+  if (!partnerById) {
+    return { type: 'error', message: 'Partner Not Found', status: 404 };
+  }
+  return { type: null, message: partnerById, status: 200 };
+};
+
 module.exports = {
   getAllPartners,
+  getPartnerById,
 };
