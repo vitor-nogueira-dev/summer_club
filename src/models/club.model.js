4c1c253c0b2c8d55a8ca0c57b15331b463cdbcd9
Vitor Nogueira <vslnogueira96@gmail.com>
2023-10-25 09:21:30 -0300

    feat: creating model create partner

--- src/models/club.model.js
+++ src/models/club.model.js
@@ -1,16 +1,33 @@
 const connection = require('../db/connection');
 
 const getAllPartners = async () => {
-  const [result] = await connection.execute('SELECT * FROM summer_club.partners');
+  const [result] = await connection.execute(
+    'SELECT * FROM summer_club.partners',
+  );
   return result;
 };
 
 const getPartnerById = async (id) => {
-  const [[result]] = await connection.execute('SELECT * FROM summer_club.partners WHERE id = ?', [id]);
+  const [[result]] = await connection.execute(
+    'SELECT * FROM summer_club.partners WHERE id = ?',
+    [id],
+  );
   return result;
+};
+
+const createPartner = async (partner) => {
+  const {
+    name, age, email, matricula, frequent,
+  } = partner;
+  const [{ insertId }] = await connection.execute(
+    'INSERT INTO summer_club.partners (name, age, email, matricula, frequent) VALUES (?, ?, ?, ?, ?)',
+    [name, age, email, matricula, frequent],
+  );
+  return insertId;
 };
 
 module.exports = {
   getAllPartners,
   getPartnerById,
+  createPartner,
 };
