ce1a40c8d9db1de82260c50a44656fdf813c1018
Vitor Nogueira <vslnogueira96@gmail.com>
2023-11-04 09:45:47 -0300

    test: creating integration test get partner by id

---
+++ tests/integration/integration.test.js
@@ -0,0 +1,39 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+const chaiHTTP = require('chai-http');
+const mocks = require('../mocks/mocks');
+const service = require('../../src/services');
+
+const app = require('../../src/app');
+
+chai.use(sinonChai);
+chai.use(chaiHTTP);
+
+const { expect } = chai;
+
+describe.only('Teste de integração [GET, POST, PUT, DELETE]', () => {
+  afterEach(() => sinon.restore());
+  it('Testando partner by id', async () => {
+    // arrange
+    sinon
+      .stub(service, 'getPartnerById')
+      .resolves({ type: null, message: mocks.getPartnerById, status: 200 });
+    // act
+    const response = await chai.request(app).get('/club/partners/2');
+    // assert
+    expect(response.status).to.be.equal(200);
+    expect(response.body).to.be.deep.equal(mocks.getPartnerById);
+  });
+  it('Testando se partner by id retorna um erro quando o sócio não existe', async () => {
+    // arrange
+    sinon
+      .stub(service, 'getPartnerById')
+      .resolves({ type: 'error', message: 'Partner Not Found', status: 404 });
+    // act
+    const response = await chai.request(app).get('/club/partners/1000');
+    // assert
+    expect(response.status).to.be.equal(404);
+    expect(response.body).to.be.deep.equal({ message: 'Partner Not Found' });
+  });
+});
